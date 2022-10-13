60f6a81255d7948ac55ffa77b4a5364f71ac5131
AndriiFedoriv <56352523+AndriiFedoriv@users.noreply.github.com>
2022-10-13 16:57:26 +0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
@@ -9,7 +10,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatToolbarModule, MatIconModule ],
+  imports:      [ BrowserModule, FormsModule, MatToolbarModule, MatIconModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+    ])
+   ],
   declarations: [ AppComponent, ToolbarComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })

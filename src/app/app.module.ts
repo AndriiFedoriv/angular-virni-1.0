b88767707c939daa22392ac22bf8b0a6ec55d628
AndriiFedoriv <56352523+AndriiFedoriv@users.noreply.github.com>
2022-10-16 15:48:35 +0300

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import {MaterialExampleModule} from '../material.module';
@@ -9,14 +10,15 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
 import { HomeComponent } from './home/home.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatNativeDateModule} from '@angular/material/core';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MaterialExampleModule,  MatToolbarModule, MatIconModule,
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, MaterialExampleModule,  MatToolbarModule, MatIconModule, ReactiveFormsModule, MatNativeDateModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
     ])
    ],
   declarations: [ AppComponent, ToolbarComponent, HomeComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent, ToolbarComponent ]
 })
 export class AppModule { }

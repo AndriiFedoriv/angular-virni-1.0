c76a5522d633bb5b1e8d278d1cd6ea035d291bd0
AndriiFedoriv <56352523+AndriiFedoriv@users.noreply.github.com>
2022-10-17 17:08:00 +0300

    2 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
 
   constructor() { }
 
